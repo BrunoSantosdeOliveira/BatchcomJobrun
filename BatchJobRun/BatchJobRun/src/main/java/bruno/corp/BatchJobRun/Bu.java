630ca8046c01f16ee2d9f62887891c1c4978bebf
BrunoSantosdeOliveira <84398016+BrunoSantosdeOliveira@users.noreply.github.com>
2022-05-31 23:33:20 -0300

    Persistencia Banco de Dados
    Persistencia Banco de Dados

---
+++ BatchJobRun/BatchJobRun/src/main/java/bruno/corp/BatchJobRun/Bu.java
@@ -0,0 +1,14 @@
+package bruno.corp.BatchJobRun;
+
+import org.jobrunr.configuration.JobRunr;
+import org.jobrunr.server.JobActivator;
+import org.jobrunr.storage.sql.common.SqlStorageProviderFactory;
+import org.springframework.context.annotation.Bean;
+import org.jobrunr.scheduling.JobScheduler;
+import org.jobrunr.*;
+
+import javax.sql.DataSource;
+
+public class Bu {
+
+}
